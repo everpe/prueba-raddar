41e5351167659e7bd8484ff9047a6f3e8dcd2bc2
everpe <ever.penab@autonoma.edu.co>
2024-12-27 22:26:44 -0500

    bug service

--- src/app/services/tiny-url.service.ts
+++ src/app/services/tiny-url.service.ts
@@ -1,15 +1,13 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CreateTinyUrlRequest, TinyUrlResponse } from '../interfaces/Tiny.Interface';
-
-
+import { CreateTinyUrlRequest, DecodeTinyUrlResponse, TinyUrlResponse } from '../interfaces/Tiny.Interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TinyUrlService {
-  private apiUrl = 'https://api.tinyurl.com/create';
+  private apiUrl = 'https://api.tinyurl.com';
   private apiToken = '3vBkgC8bxEhQ0hFSTB1kYVuJ1NJn5XMDO4NW0SYrpVe7VXH2cnZEYeUB39H2';
 
   constructor(private http: HttpClient) {}
@@ -22,15 +20,15 @@ export class TinyUrlService {
       'Content-Type': 'application/json',
     });
 
-    return this.http.post<TinyUrlResponse>(`${this.apiUrl}?${params.toString()}`, request, { headers });
+    return this.http.post<TinyUrlResponse>(`${this.apiUrl}/create?${params.toString()}`, request, { headers });
   }
   
-  decode(domain: string, alias: string): Observable<any> {
-    const headers = {
-      Authorization: `Bearer ${this.apiToken}`,
-    };
-  
-    return this.http.get(`${this.apiUrl}/alias/${domain}/${alias}`, { headers });
-  }
-  
+// Método para decodificar una URL corta
+decode(domain: string, alias: string): Observable<DecodeTinyUrlResponse> {
+  const params = new HttpParams().set('api_token', this.apiToken);
+
+  return this.http.get<DecodeTinyUrlResponse>(
+    `${this.apiUrl}/alias/${domain}/${alias}?${params.toString()}`
+  );
+}
 }
